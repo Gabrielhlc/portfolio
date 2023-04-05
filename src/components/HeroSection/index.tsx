d7df3980f656fffef27faeddd7829e5ce4a6dab3
Gabrielhlc <gabrielhenlc@gmail.com>
2023-04-05 12:20:29 -0300

    add: hero rotation animation

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,16 +1,41 @@
+import Image from "next/image";
 import { Button } from "../Button";
-import { ButtonContainer, Container, Subtitle, Title } from "./styles";
+import { ButtonContainer, Container, InfoWrapper, Orbit1, Orbit2, Orbit3, OrbitWrapper, Profile, ProfileContainer, Subtitle, Title } from "./styles";
+
+import reactSvg from '../../assets/react.svg';
+import typescriptSvg from '../../assets/typescript.svg';
+import nextSvg from '../../assets/next.svg';
 
 export function HeroSection() {
     return (
         <Container>
-            <Title>Welcome to my portfolio page!</Title>
+            <InfoWrapper>
+                <Title>Welcome to my portfolio page!</Title>
 
-            <Subtitle>My name is Gabriel Henrique Lopes Carvalho and I'm a Frontend developer</Subtitle>
+                <Subtitle>My name is Gabriel Henrique Lopes Carvalho and I'm a Frontend developer</Subtitle>
 
-            <ButtonContainer>
-                <Button size="md" text="Let's begin!" />
-            </ButtonContainer>
+                <ButtonContainer>
+                    <Button size="md" text="Let's begin!" />
+                </ButtonContainer>
+            </InfoWrapper>
+
+
+            <ProfileContainer>
+                <Profile>
+                    <Image src="https://github.com/Gabrielhlc.png" alt="Profile" width={350} height={350} />
+                </Profile>
+                <OrbitWrapper>
+                    <Orbit1>
+                        <Image src={reactSvg} alt="React logo" width={70} height={70} />
+                    </Orbit1>
+                    <Orbit2>
+                        <Image src={reactSvg} alt="React logo" width={70} height={70} />
+                    </Orbit2>
+                    <Orbit3>
+                        <Image src={reactSvg} alt="React logo" width={70} height={70} />
+                    </Orbit3>
+                </OrbitWrapper>
+            </ProfileContainer>
         </Container>
     )
 }
--- src/components/HeroSection/styles.ts
+++ src/components/HeroSection/styles.ts
@@ -1,9 +1,15 @@
-import styled from "styled-components";
+import styled, { Keyframes, keyframes } from "styled-components";
 
 export const Container = styled.div`
     margin-left: 60px;
     margin-bottom: 150px;
 
+    display: flex;
+    align-items: center;
+`;
+
+export const InfoWrapper = styled.div`
+    width: 70%;
     display: flex;
     flex-direction: column;
 `;
@@ -16,7 +22,7 @@ export const Title = styled.h1`
 export const Subtitle = styled.h2`
     margin-left: 3.875rem;
     margin-bottom: 4.5rem;
-    width: 54%;
+    width: 80%;
 
     font-size: 2.25rem;
     color: ${({ theme }) => theme.gray};
@@ -24,4 +30,53 @@ export const Subtitle = styled.h2`
 
 export const ButtonContainer = styled.div`
     margin-left: 6.75rem;
+`;
+
+export const ProfileContainer = styled.div`
+    position: relative;
+`;
+
+export const Profile = styled.div`
+    position: relative;
+    z-index: 2;
+    display: flex;
+    border-radius: 50%;
+    overflow: hidden;
+    box-shadow: 0px 5px 34px 5px rgba(0, 0, 0, 0.5);
+`;
+
+const rotate = keyframes`
+  0% {
+    transform: translate(-50%, -50%) rotate(-360deg);
+  }
+`;
+
+
+export const OrbitWrapper = styled.div`
+  z-index: 3;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
+  animation: ${rotate} 4s linear infinite;
+`;
+
+export const Orbit1 = styled.div`
+    position: absolute;
+    left: 135px;
+    top: -80px;
+`;
+
+export const Orbit2 = styled.div`
+    position: absolute;
+    top: 300px;
+    left: -20px;
+`;
+export const Orbit3 = styled.div`
+    position: absolute;
+    top: 300px;
+    right: -20px;
 `;
